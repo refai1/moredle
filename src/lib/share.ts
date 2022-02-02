7023e7b2e80158fe863d0e6fea46e332a16aaee3
Ahmed Refai <refai241@gmail.com>
2022-02-02 08:40:35 -0800

    share button attempt

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -1,12 +1,18 @@
 import { getGuessStatuses } from './statuses'
 import { solutionIndex } from './words'
 import { WORDLE_TITLE } from '../constants/strings'
+import {isMobile} from 'react-device-detect';
 
 export const shareStatus = (guesses: string[], lost: boolean) => {
-  navigator.clipboard.writeText(
-    `${WORDLE_TITLE} ${solutionIndex+1} ${lost ? 'X' : guesses.length}/6\n\n` +
-      generateEmojiGrid(guesses)
-  )
+  const mainText = `${WORDLE_TITLE} ${solutionIndex+1} ${lost ? 'X' : guesses.length}/6\n\n` + generateEmojiGrid(guesses);
+  if (isMobile && navigator.share){
+    navigator.share({
+      title: "Moredle",
+      text: mainText,
+    })
+  } else {
+    navigator.clipboard.writeText(mainText)
+  }
 }
 
 export const generateEmojiGrid = (guesses: string[]) => {
